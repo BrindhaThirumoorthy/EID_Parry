7329db1b33e151dfdf3c9fda9f3dbce8b93c0ec1
PIM-EID-Infra <pimeidinfra@eid.com>
2024-11-27 18:06:02 +0530

    new branch

--- Tests/Resource/SAP_Tcode_Library.py
+++ Tests/Resource/SAP_Tcode_Library.py
@@ -1423,7 +1423,7 @@ class SAP_Tcode_Library:
                     os.remove(file)
         except Exception as e:
             print(f"An error occurred: {e}")
-            
+
     def get_sap_table_value(self, table_id, row_num, column_id):
         # Get Sap Table Value    table_id=wnd[0]/usr/cntlGRID1/shellcont/shell    row_num=${row_num}    column_id=BELNR
         try:
@@ -1433,3 +1433,13 @@ class SAP_Tcode_Library:
             return cell_value  
         except Exception as e:
                 return "FAIL"
+        
+    def delete_specific_file(self, file_path):
+            try:
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+                else:
+                    print(f"The file '{file_path}' does not exist.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
