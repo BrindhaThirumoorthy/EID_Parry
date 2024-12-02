5c21385ab4f61dec287fba526771708146196ea4
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-02 14:34:09 +0530

    new chnges

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -4,6 +4,6 @@ Rental_Client = "900"
 Rental_User = "bcssupport1"
 Rental_Start_Date ="01.11.2024"
 Rental_End_Date = "30.11.2024"
-documents = ["40025331","40025332","40025408"]
+documents = "["\"40025131\""]"
 Invoice_PDF_PATH = "C:\\TEMP\\"
 search_terms = ["Customer Reference","Document Date","Sales Document","Sales Document Type","Sales Document Item","Material","Target Quantity","Sales Unit","Valid-To Date","Sold-to Party","Net Value (Item)","Document Currency","Created By","Contract Start Date","Contract End Date",]
