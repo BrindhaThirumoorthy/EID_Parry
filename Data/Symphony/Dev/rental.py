a21067966deedc772288e1204311a715393b3bf8
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-03 15:48:46 +0530

    new changes

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -3,5 +3,5 @@ Rental_Connection = "EID sandbox"
 Rental_Client = "900"
 Rental_User = "bcssupport1"
 Layout = ["Contracts - Header",]
-documents = ["40025134","40025323","40025325"]
+documents = "40025134,40025323,40025325"
 search_terms = ["Customer Reference","Document Date","Sales Document","Sales Document Type","Sales Document Item","Material","Target Quantity","Sales Unit","Valid-To Date","Sold-to Party","Net Value (Item)","Document Currency","Created By","Contract Start Date","Contract End Date",]
--- Tests/Resource/output.py
+++ Tests/Resource/output.py
@@ -2,4 +2,8 @@ import sys
 
 output = sys.argv[1]
 
-print(f"##gbStart##document_number##splitKeyValue##{output}##gbEnd##")
+if len(sys.argv) == 2:
+    print(f"##gbStart##document_number##splitKeyValue##{output}##gbEnd##")
+else:
+    print("Please supply all the arguments.")
+    sys.exit(1)
