a6306f728eee8e011427e0e3a6cd36c0467d273c
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-04 16:41:08 +0530

    new changes

--- Tests/Resource/Dumps.py
+++ Tests/Resource/Dumps.py
@@ -1,18 +1,22 @@
-import  json
+import json
 import sys
-print(sys.argv)
-# list_value = "40025323, 40025326,40025328,40025330"
-list_value = sys.argv[1]
 
-list_new = [int(i.strip()) for i in list_value.split(",")]
+# sys.argv = ["script_name", '["Sales Document: 40026233 | Sold-To-Party: 800597", "Sales Document: 40026234 | Sold-To-Party: 800597"]']
+print(sys.argv)
+
+# Extracting the input JSON array from the arguments
+input_array = json.loads(sys.argv[1])
+
+# Extracting Sales Document values
+list_new = [int(item.split('|')[0].split(':')[1].strip()) for item in input_array]
 print(list_new, type(list_new))
 
-# list_value = list(list_value)
+# Creating the document body structure
+document_body = [{"document": f"{i}"} for i in list_new]
 
-document_body = [{"document":f"{i}"} for i in list_new]
+# Converting to JSON
+document_json = json.dumps(document_body, separators=(',', ':'))
 
-document_json = json.dumps(document_body, separators=(',',':'))
-
+# Printing the output in the specified format
 print(f"##gbStart##document_json##splitKeyValue##{document_json}##splitKeyValue##object##gbEnd##")
-# print(f"##gbStart##document_json##splitKeyValue##{document_json}##gbEnd##")
 print("Script Processed Successfully")
