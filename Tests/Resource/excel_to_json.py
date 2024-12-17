d1fce7095154c553affadd12ab945d8c3e7deb0e
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-17 16:12:55 +0530

    email notification changes

--- Tests/Resource/excel_to_json.py
+++ Tests/Resource/excel_to_json.py
@@ -4,27 +4,33 @@ import os
 from datetime import time
 
 def convert_excel_to_json(excel_file, json_file):
-    # Read the Excel file
     df = pd.read_excel(excel_file, engine='openpyxl')
     df = df.where(pd.notnull(df), "empty")
-
-    # Convert datetime and time objects to strings
+    for column in df.select_dtypes(['datetime']):
+        df[column] = df[column].astype(str)
     for column in df.columns:
-        if pd.api.types.is_datetime64_any_dtype(df[column]):
-            df[column] = df[column].astype(str)
-        elif pd.api.types.is_timedelta64_dtype(df[column]):
-            df[column] = df[column].astype(str)
-        elif pd.api.types.is_object_dtype(df[column]):
-            df[column] = df[column].apply(
-                lambda x: x.strftime('%H:%M:%S') if isinstance(x, time) else x
-            )
-    
-    # Convert the DataFrame to a dictionary
+        if df[column].dtype == 'object': 
+            df[column] = df[column].str.strip('"') 
     data = df.to_dict(orient='records')
-    
-    # Write the dictionary to a JSON file
     with open(json_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+    with open(json_file, 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+    return json_data
+    # for column in df.columns:
+    #     if pd.api.types.is_datetime64_any_dtype(df[column]):
+    #         df[column] = df[column].astype(str)
+    #     elif pd.api.types.is_timedelta64_dtype(df[column]):
+    #         df[column] = df[column].astype(str)
+    #     elif pd.api.types.is_object_dtype(df[column]):
+    #         df[column] = df[column].apply(
+    #             lambda x: x.strftime('%H:%M:%S') if isinstance(x, time) else x
+    #         )
+    # data = df.to_dict(orient='records')
+    
+    # # Write the dictionary to a JSON file
+    # with open(json_file, 'w', encoding='utf-8') as f:
+    #     json.dump(data, f, ensure_ascii=False, indent=4)
 
 def read_json(json_file):
     # Open and read the JSON file
@@ -44,8 +50,8 @@ def delete_files(files):
 # excel_file = r'C:\\Output\\Rental_output.xlsx'  # Replace with your Excel file path
 # json_file = r'C:\\Output\\Rental_output.json'  # Replace with your desired JSON file path
 
-# convert_excel_to_json(excel_file, json_file)
-# data = read_json(json_file)
+# data = convert_excel_to_json(excel_file, json_file)
+# # data = read_json(json_file)
 # print(f"##gbStart##copilot_key##splitKeyValue##{data}##splitKeyValue##object##gbEnd##")
 
 # Delete the files
