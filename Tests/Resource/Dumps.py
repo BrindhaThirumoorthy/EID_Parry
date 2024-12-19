4fd3e1c31c91e6fa2e5d1926a0112ae6ec8fde8a
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-19 12:46:43 +0530

    dump.py

--- Tests/Resource/Dumps.py
+++ Tests/Resource/Dumps.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-# sys.argv = ["script_name", '[Sales Document: 40026233 | Sold-To-Party: 800597, Sales Document: 40026234 | Sold-To-Party: 800597]']
+# sys.argv = ["Sales Document: 40026198 | Sold To Party: MURUGAPPA MANAGEMENT SERVICES | Billing Block: nan","Sales Document: 40025941 | Sold To Party: Cholamandalam MS Risk Services Limited | Billing Block: Compl Confirm Missng"]
 print("Arguments received:", sys.argv)
 
 # Extract the raw input string from the arguments
@@ -9,14 +9,19 @@ raw_input = sys.argv[1]
 
 input_items = raw_input.strip("[]").split(",")
 
+# list_new = []
+# for item in input_items:
+#     # Remove leading/trailing spaces
+#     item = item.strip()
+#     # Parse the "Sales Document" value
+#     if "Sales Document:" in item:
+#         sales_document = item.split('|')[0].split(':')[1].strip()
+#         list_new.append(int(sales_document))
 list_new = []
 for item in input_items:
-    # Remove leading/trailing spaces
     item = item.strip()
-    # Parse the "Sales Document" value
-    if "Sales Document:" in item:
-        sales_document = item.split('|')[0].split(':')[1].strip()
-        list_new.append(int(sales_document))
+    list_new.append(int(item))
+    
 
 print("Extracted Sales Document list:", list_new, type(list_new))
 
