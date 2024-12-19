dbe3f67cddb3f62c4d77087113d7e9a670478b29
PIM-EID-Infra <pimeidinfra@eid.com>
2024-12-19 13:45:48 +0530

    dump.py

--- Data/Symphony/Dev/rental.py
+++ Data/Symphony/Dev/rental.py
@@ -1,7 +1,7 @@
 SAP_SERVER = "C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\saplogon.exe"
 Rental_Connection = "EID sandbox"
 Rental_Client = "900"
-Rental_User = "bcssupport1"
+Rental_User = "eecfgn03"
 DateContent = {"startDate":"01.12.2024","endDate":"31.12.2024"}
 Layout = ["Contracts - Header",]
-search_terms = ["Customer Reference","Document Date","Sales Document","Sales Document Type","Sales Document Item","Material","Target Quantity","Sales Unit","Valid-To Date","Sold-to Party","Net Value (Item)","Document Currency","Created By","Contract Start Date","Contract End Date",]
+search_terms = ["Sold-To Party Name","Billing Block Description"]
--- Tests/Test_VA45.robot
+++ Tests/Test_VA45.robot
@@ -1,10 +1,10 @@
 *** Settings ***
 Resource    ../Tests/Resource/VA45.robot 
-Suite Setup    VA45.System Logon
+# Suite Setup    VA45.System Logon
 Suite Teardown    VA45.System Logout
 Test Tags    VA45
  
  
 *** Test Cases ***
 Display the rental documents
-    Rental Document
+    System Logon
